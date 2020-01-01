ed083029ab4b0fe96e730bfb29a94cec80fbc49a
Michael Yeates <michaeljyeates@gmail.com>
2020-01-01 22:15:36 +0000

    Update constitution layout

--- src/pages/custodian/manage-constitution.vue
+++ src/pages/custodian/manage-constitution.vue
@@ -4,163 +4,122 @@
       Review the constitution history and propose constitution updates.
     </p>
     <div class="">
-      <div class="row gutter-sm ">
+      <div class="row q-col-gutter-md">
         <div class="col-xs-12 col-md-5 animate-fade">
-          <div class="round-borders bg-bg1 shadow-4 overflow-hidden">
-            <div
-              class="bg-primary q-pa-sm row justify-between items-center"
-              style="height:50px"
-            >
-              <q-icon
-                :name="$configFile.icon.dactoken"
-                color="text2"
-                size="24px"
-              />
-              <span>Constitution History</span>
-              <q-btn
-                @click="
-                  constitution_version_descending = !constitution_version_descending
-                "
-                flat
-              >
-                <q-icon
-                  color="text2"
-                  v-if="constitution_version_descending"
-                  name="mdi-sort-descending"
-                />
-                <q-icon v-else name="mdi-sort-ascending" color="text2" />
-              </q-btn>
-            </div>
+          <q-card>
+            <q-card-section class="bg-primary q-pa-xs">
+              <q-item>
+                <q-item-section avatar>
+                  <q-icon :name="$configFile.icon.dactoken" />
+                </q-item-section>
+                <q-item-section class="text-h6">
+                  Constitution History
+                </q-item-section>
+              </q-item>
 
-            <q-scroll-area
-              style="width: 100%; height: 250px;"
-              :thumb-style="{
-                right: '0px',
-                borderRadius: '2px',
-                background: '#7c41ba',
-                width: '10px',
-                opacity: 0.8
-              }"
-            >
-              <q-list no-border separator highlight>
-                <q-item
-                  v-for="(mt, i) in getParsedMemberTerms"
-                  :key="`mt${i}`"
-                  class="animate-fade"
-                >
-                  <q-item-section side>
-                    <q-icon :name="$configFile.icon.constitution" />
-                  </q-item-section>
-                  <q-item-section>
-                    <q-item-label>
-                      <div class="overflow-hidden">
-                        <span>Version {{ mt.version }} </span>
-                        <span class="q-caption text-text2">{{ mt.hash }}</span>
-                      </div>
-                    </q-item-label>
-                    <q-item-label caption>
-                      <div
-                        style="white-space: nowrap;"
-                        class=" overflow-hidden"
-                      >
-                        <a
-                          class=" q-caption"
-                          target="_blank"
-                          :href="mt.terms"
-                          >{{ mt.terms }}</a
+            </q-card-section>
+
+            <q-card-section>
+                <q-list no-border separator highlight>
+                  <q-item v-for="(mt, i) in getParsedMemberTerms" :key="`mt${i}`">
+                    <q-item-section side>
+                      <q-icon :name="$configFile.icon.constitution" />
+                    </q-item-section>
+                    <q-item-section>
+                      <q-item-label>
+                        <div class="overflow-hidden">
+                          <span>Version {{ mt.version }} </span>
+                          <span class="q-caption text-text2">{{ mt.hash }}</span>
+                        </div>
+                      </q-item-label>
+                      <q-item-label caption>
+                        <div
+                                style="white-space: nowrap;"
+                                class=" overflow-hidden"
                         >
-                      </div>
-                    </q-item-label>
-                  </q-item-section>
-                  <q-item-section side style="min-width:65px">
-                    <q-btn
-                      v-if="mt.hash != md5_constitution"
-                      color="info"
-                      class="animate-fade"
-                      flat
-                      size="sm"
-                      label="load"
-                      @click="setConstitution(mt.terms)"
-                    />
-                    <q-icon
-                      v-else
-                      name="check"
-                      size="24px"
-                      color="positive"
-                      class="animate-fade"
-                    />
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-scroll-area>
-          </div>
+                          <a
+                                  class=" q-caption"
+                                  target="_blank"
+                                  :href="mt.terms"
+                          >{{ mt.terms }}</a
+                          >
+                        </div>
+                      </q-item-label>
+                    </q-item-section>
+                    <q-item-section side style="min-width:65px">
+                      <q-btn
+                              v-if="mt.hash != md5_constitution"
+                              color="info"
+                              class="animate-fade"
+                              flat
+                              size="sm"
+                              label="load"
+                              @click="setConstitution(mt.terms)"
+                      />
+                      <q-icon
+                              v-else
+                              name="check"
+                              size="24px"
+                              color="positive"
+                              class="animate-fade"
+                      />
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+            </q-card-section>
+          </q-card>
         </div>
         <div class="col-xs-12 col-md-7 animate-fade">
-          <div
-            class="round-borders bg-bg1 bg-logo shadow-4  full-height overflow-hidden"
-          >
-            <div
-              class="bg-primary q-pa-sm row justify-between items-center"
-              style="height:50px"
-            >
-              <q-icon
-                :name="$configFile.icon.dactoken"
-                color="text2"
-                size="24px"
-              />
-              <span>New Constitution</span>
-              <help-btn
-                :content="
-                  `Input an URL pointing to the updated constitution. Click load to pull in the text and calculate its hash.`
-                "
-                :title="`New Constitution`"
-                color="text1"
-                size="sm"
-              />
-            </div>
-            <div class="q-pa-md column justify-between " style="height:250px">
-              <div class="row items-center no-wrap">
-                <q-input
-                  class="full-width "
-                  color="primary-light"
-                  v-model="new_constitution_url"
-                  stack-label="Constitution URL"
-                  placeholder="Input URL to Constitution"
-                  @input="
-                    md5_constitution = '';
-                    parsed_constitution = '';
-                  "
-                />
-                <div>
-                  <q-btn
-                    label="load"
-                    @click="setConstitution(new_constitution_url)"
-                    color="primary"
-                    :loading="isloading"
-                    class="on-right animate-fade"
+
+          <q-card>
+            <q-card-section class="bg-primary q-pa-xs">
+              <q-item>
+                <q-item-section avatar>
+                  <q-icon :name="$configFile.icon.dactoken" />
+                </q-item-section>
+                <q-item-section class="text-h6">
+                  New Constitution
+                </q-item-section>
+              </q-item>
+
+            </q-card-section>
+
+            <q-card-section>
+              <div class="q-pa-md column justify-between " style="height:250px">
+                <div class="row items-center no-wrap">
+                  <q-input
+                          class="full-width "
+                          color="primary-light"
+                          v-model="new_constitution_url"
+                          stack-label="Constitution URL"
+                          placeholder="Input URL to Constitution"
+                          @input="urlChanged"
                   />
+                  <div>
+                    <q-btn
+                            label="load"
+                            @click="setConstitution(new_constitution_url)"
+                            color="primary"
+                            :loading="isloading"
+                            class="on-right animate-fade"
+                    />
+                  </div>
+                </div>
+                <div class="row no-wrap items-center" v-if="getIsNewConstitution === true">
+                  <div>
+                    <q-btn label="Update" @click="updateConstitution" color="primary" />
+                  </div>
+                  <div class="q-caption text-positive q-pa-sm">
+                    New constitution detected. Please review the constitution and
+                    verify the md5 hash before updating. The constitution change
+                    will be put up for voting via msig.
+                  </div>
                 </div>
               </div>
-              <div
-                class="row no-wrap items-center"
-                v-if="getIsNewConstitution === true"
-              >
-                <div>
-                  <q-btn
-                    label="Update"
-                    @click="updateConstitution"
-                    color="primary"
-                  />
-                </div>
-                <div class="q-caption text-positive q-pa-sm">
-                  New constitution detected. Please review the constitution and
-                  verify the md5 hash before updating. The constitution change
-                  will be put up for voting via msig.
-                </div>
-              </div>
-            </div>
-          </div>
+            </q-card-section>
+          </q-card>
         </div>
+
         <div class="col-xs-12 animate-scale">
           <div class="q-pa-md round-borders bg-bg1 shadow-4">
             <div v-if="parsed_constitution != ''">
@@ -168,14 +127,11 @@
                 <xspan :value="md5_constitution" />
               </div>
               <div
-                class="markdown-body animate-fade bg-white q-pa-md"
+                class="markdown-body animate-fade q-pa-md"
                 v-html="parsed_constitution"
               />
             </div>
-            <div
-              v-if="!isloading && parsed_constitution == ''"
-              class="animate-fade text-text2"
-            >
+            <div v-if="!isloading && parsed_constitution == ''">
               No constitution loaded
             </div>
             <div v-if="isloading" class="animate-fade">loading...</div>
@@ -275,6 +231,11 @@ export default {
       } catch (err) {
         return false
       }
+    },
+
+    urlChanged () {
+      this.md5_constitution = ''
+      this.parsed_constitution = ''
     },
 
     async updateConstitution () {
